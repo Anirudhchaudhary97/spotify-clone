398448b95db68436c53c7deb7ebe8636bc3fcede
Anirudh Chaudhary <anurudhchaudhary97@gmail.com>
2024-05-17 00:07:27 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import SideBar from './components/SideBar'
+import Player from './components/Player'
+import Display from './components/Display'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+     <SideBar/>
+     <Display/>
+      </div>
+      <div>
+        <Player/>
+      </div>
+    </div>
+  )
+}
+
+export default App
